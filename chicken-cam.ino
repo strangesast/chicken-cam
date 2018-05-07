e394f6459f631c7fcc6873f3b2a7603d50ebc9e3
Sam Zagrobelny <strangesast@gmail.com>
2018-05-06 23:32:59 -0400

    partial

--- chicken-cam.ino
+++ chicken-cam.ino
@@ -5,6 +5,11 @@
 OneWire oneWire(ONE_WIRE_BUS); 
 DallasTemperature sensors(&oneWire);
 
+// inside
+const uint8_t id1[8] = {0x28, 0xFF, 0x69, 0xD6, 0x63, 0x15, 0x02, 0x4C};
+// outside
+const uint8_t id2[8] = {0x28, 0xFF, 0x3F, 0xBF, 0x14, 0x14, 0x00, 0xAB};
+
 void setup(void) 
 { 
  Serial.begin(9600);
@@ -13,6 +18,8 @@ void setup(void)
 void loop(void) 
 { 
  sensors.requestTemperatures();
- Serial.println(sensors.getTempCByIndex(0) * 9 / 5 + 32);
+ Serial.print(sensors.getTempF(id1));
+ Serial.print(',');
+ Serial.println(sensors.getTempF(id2));
  delay(1000); 
 }
--- server.js
+++ server.js
@@ -9,8 +9,9 @@ const app = express();
 const port = new SerialPort('/dev/ttyACM0', { baudRate: 9600 });
 
 const db = new Database(path.join(__dirname, 'database'));
+let lastValue;
 // `create table if not exists temps ();`
-db.run(`create table if not exists temps (temp Integer, date Integer);`);
+db.run(`create table if not exists temps (inside Integer, outside Integer, date Integer);`);
 
 if (process.env.NODE_ENV !== 'production') {
   app.get('/', (_, res) => {
@@ -56,24 +57,36 @@ app.get('/api/status', (_, res) => {
   res.json({ open: port.isOpen });
 });
 
+let first = true;
+
 port.on('open', function() {
   let last = '';
   port.on('data', function(data) {
     last += data.toString()
-    const date = Date.now();
-    const arr = last.split('\r\n');
-    let add;
-    ([add, last] = [arr.slice(0, -1).map(v => Math.floor(parseFloat(v)*100)), arr.slice(-1)[0]])
-    if (add.length > 0) {
-      db.run(`insert into temps values ${add.map(temp => `(${temp},${date})`).join(',')};`);
-
-      for (const temp of add) {
-        wss.clients
-          .forEach(client => client.readyState === WebSocket.OPEN ?
-            client.send(JSON.stringify({ temp, date })) :
-            null
-          );
+    let arr = last.split('\r\n');
+    // dump first response
+    console.log(first, arr);
+    if (first) {
+      if (arr.length > 1) {
+        arr = arr.slice(1);
+        first = false;
+      } else {
+        return;
       }
+    }
+    let add;
+    ([add, last] = [arr.slice(0, -1), arr.slice(-1)[0]])
+    const date = Date.now();
+    console.log('add', add);
+    for (const temps of add) {
+      const [inside, outside] = temps.split(',').map(v => Math.floor(parseFloat(v)*100));
+      lastValue = { inside, outside, date };
+      db.run(`insert into temps values (${[inside, outside, date].join(',')})`);
+      wss.clients
+        .forEach(client => client.readyState === WebSocket.OPEN ?
+          client.send(JSON.stringify({ inside, outside, date })) :
+          null
+        );
     }
   });
 });
