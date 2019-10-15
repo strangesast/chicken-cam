d1218a478a0857915f2c958fb18167a73454e863
Sam Zagrobelny <strangesast@gmail.com>
2019-10-15 17:33:50 -0400

    another convenience script

--- scripts/query.sh
+++ scripts/query.sh
@@ -1,2 +1,3 @@
 #select value, strftime('%d-%m-%Y %H:%M', datetime(date, 'unixepoch', 'localtime')) from requests;
 #sqlite3 test.db "select (date*1.0-strftime('%s', 'now'))/3600 from requests";
+select datetime(date, 'unixepoch', 'localtime') from requests where textstate=="scheduled" limit 1;
