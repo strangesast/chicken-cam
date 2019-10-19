31893446a6f35bafe82e055f261ddb8501607320
Sam Zagrobelny <strangesast@gmail.com>
2019-10-19 17:01:38 -0400

    missed from last commit

--- server/server.py
+++ server/server.py
@@ -3,10 +3,78 @@ import sys
 import asyncio
 import logging
 import aiosqlite
-from datetime import datetime
 from systemd.journal import JournalHandler
 
 from common import init_db
+
+import os
+from os.path import dirname, realpath, normpath, join
+import json
+import asyncio
+import aiosqlite
+from aiohttp import web
+from datetime import datetime, timedelta
+from pathlib import Path
+
+from common import init_db
+
+
+routes = web.RouteTableDef()
+
+QUERY_DEFAULTS = {
+    'offset': 0,
+    'limit': 100,
+    'range': 14
+}
+
+
+# get changes history
+@routes.get('/state')
+async def get_states(request: web.Request):
+    db: aiosqlite.Connection = request.app['db']
+    offset, limit, _range = [int(v) if (v := request.rel_url.query.get(s, '')).isdigit()
+            else QUERY_DEFAULTS[s] for s in ['offset', 'limit', 'range']]
+    # by default today - this week - last week
+    gt = int((datetime.now() - timedelta(days=14)).timestamp())
+    keys = ['date','topsensor','sidesensor','bottomsensor']
+    async with db.execute('SELECT {} FROM events_states'.format(','.join(keys))) as cursor:
+        records = []
+        last = None
+        async for d,*v in cursor:
+            if last != v:
+                last = v
+                d = datetime.fromtimestamp(d).isoformat()
+                records.append(dict(zip(keys,[d]+v)))
+    return web.json_response({'records': records})
+
+
+@routes.get('/changes')
+async def get_changes(request: web.Request):
+    db: aiosqlite.Connection  = request.app['db']
+    records = []
+    keys = ['date','fromstate','tostate']
+    async with db.execute('SELECT {},{},{} FROM events_changes'.format(*keys)) as cursor:
+        async for d,*v in cursor:
+            d = datetime.fromtimestamp(d).isoformat()
+            records.append(dict(zip(keys, [d]+v)))
+    return web.json_response({'records': records})
+
+
+@routes.get('/requests')
+async def get_requests(request):
+    requests = []
+    db = request.app['db']
+    keys = ['date','completed','created','id','value','textstate']
+    async with db.execute('SELECT {} FROM requests'.format(','.join(keys))) as cursor:
+        async for row in cursor:
+            values = [datetime.fromtimestamp(d).isoformat() if d else None for d in row[0:3]] + list(row[3:])
+            requests.append(dict(zip(keys,values)))
+
+    return web.json_response({'requests': requests})
+
+
+def split_nums(s, delim):
+    return [int(v) if v.isdigit() else None for v in s.split(delim)]
 
 
 async def reader_handler(reader, db: aiosqlite.Connection):
@@ -17,22 +85,20 @@ async def reader_handler(reader, db: aiosqlite.Connection):
                 b = await asyncio.wait_for(reader.readline(), timeout=5)
             except asyncio.TimeoutError:
                 break
-            date = datetime.now()
+            date = int(datetime.now().timestamp())
             raw = b.decode('utf8', errors='ignore').strip()
-            if '->' in raw:
+            if '->' in raw and len(values := split_nums(raw, '->')) == 2:
                 # fromstate, tostate
-                values = [int(v) if v.isdigit() else None for v in raw.split('->')]
                 q = con.execute('''INSERT INTO events_changes
-                (date,raw,fromstate,tostate) VALUES (?,?,?,?)''', (date, raw, *values))
-            elif '|' in raw:
+                        (date,raw,fromstate,tostate) VALUES (?,?,?,?)''', (date, raw, *values))
+            if '|' in raw and len(values := split_nums(raw, '|')) == 4:
                 # currentstate, topsensor, sidesensor, bottomsensor
-                values = [int(v) if v.isdigit() else None for v in raw.split('|')]
                 q = db.execute('''INSERT INTO events_states
-                (date,raw,currentstate,topsensor,sidesensor,bottomsensor)
-                VALUES (?,?,?,?,?,?)''', (date, raw, *values))
+                        (date,raw,currentstate,topsensor,sidesensor,bottomsensor)
+                        VALUES (?,?,?,?,?,?)''', (date, raw, *values))
             else:
                 q = db.execute('''INSERT INTO events_unknown
-                (date,raw) VALUES (?,?)''', (date, raw))
+                        (date,raw) VALUES (?,?)''', (date, raw))
             new = True
             await q
 
@@ -50,14 +116,14 @@ async def writer_handler(writer, db: aiosqlite.Connection, queue: asyncio.Queue)
         except:
             textstate = 'failed'
         await db.execute('''UPDATE requests SET textstate = ?, completed = ?
-        WHERE id = ?''', (textstate, date, _id))
+                WHERE id = ?''', (textstate, date, _id))
 
 
 async def poller(db: aiosqlite.Connection, queue: asyncio.Queue, timeout=60):
     while True:
         date = datetime.now()
         async with db.execute('SELECT id, value FROM requests WHERE date < ? AND textstate = ?',
-                (int(datetime.timestamp()), 'scheduled')) as cursor:
+                (int(date.timestamp()), 'scheduled')) as cursor:
             command = await cursor.fetchone()
 
         if command:
@@ -67,11 +133,14 @@ async def poller(db: aiosqlite.Connection, queue: asyncio.Queue, timeout=60):
 
 
 async def main():
-    log = logging.getLogger()
-    log.addHandler(JournalHandler())
-    log.setLevel(logging.DEBUG)
+    #log = logging.getLogger()
+    #log.addHandler(JournalHandler())
+    #log.setLevel(logging.DEBUG)
 
-    db = await aiosqlite.connect(os.environ.get('DB_FILE', 'test.db'))
+
+    db_file = os.environ.get('DB_FILE', 'test.db')
+    print(f'{db_file=}')
+    db = await aiosqlite.connect(db_file)
     await init_db(db)
 
     reader, writer = await asyncio.open_connection('127.0.0.1',
@@ -79,11 +148,26 @@ async def main():
 
     queue = asyncio.Queue()
 
-    await reader.read(12) # greeting? extra bullshit
-    await asyncio.gather(
-        reader_handler(reader, db),
-        writer_handler(writer, db, queue),
-        poller(db, queue))
+    app = web.Application()
+    app['db'] = db
+    app.add_routes(routes)
+    app.router.add_static('/', dirname(realpath(__file__)))
+    
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, '0.0.0.0', 3000)
+    await site.start()
+
+    await reader.read(12) # extra bullshit from ser2net
+    try:
+        await asyncio.gather(
+            reader_handler(reader, db),
+            writer_handler(writer, db, queue),
+            poller(db, queue))
+
+    finally:
+        await runner.cleanup()
+        await db.close()
 
 
 if __name__ == '__main__':
